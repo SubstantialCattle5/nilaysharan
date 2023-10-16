28441e2989930dfb40bf6db8377496b2e9b1e349
nilay nath sharan <nilay@pop-os.localdomain>
2023-10-16 20:28:10 +0530

    fix: colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,56 +1,78 @@
-import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+        primary: ['Inter', ...fontFamily.sans],
       },
       colors: {
         primary: {
           // Customize it on globals.css :root
-          50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
-          100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
-          200: 'rgb(var(--tw-color-primary-200) / <alpha-value>)',
-          300: 'rgb(var(--tw-color-primary-300) / <alpha-value>)',
-          400: 'rgb(var(--tw-color-primary-400) / <alpha-value>)',
-          500: 'rgb(var(--tw-color-primary-500) / <alpha-value>)',
-          600: 'rgb(var(--tw-color-primary-600) / <alpha-value>)',
-          700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
-          800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
-          900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
-          950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
+          200: 'rgb(var(--tw-clr-primary-200) / <alpha-value>)',
+          300: 'rgb(var(--tw-clr-primary-300) / <alpha-value>)',
+          400: 'rgb(var(--tw-clr-primary-400) / <alpha-value>)',
+          500: 'rgb(var(--tw-clr-primary-500) / <alpha-value>)',
         },
-        dark: '#222222',
+        dark: '#0e1111',
       },
       keyframes: {
         flicker: {
           '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
-            opacity: '0.99',
+            opacity: 0.99,
             filter:
               'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
           },
           '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
-            opacity: '0.4',
+            opacity: 0.4,
             filter: 'none',
           },
         },
-        shimmer: {
-          '0%': {
-            backgroundPosition: '-700px 0',
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
           },
-          '100%': {
-            backgroundPosition: '700px 0',
+          '25%': {
+            transform: 'rotate(0.5deg)',
+          },
+          '75%': {
+            transform: 'rotate(-0.5deg)',
           },
         },
       },
       animation: {
         flicker: 'flicker 3s linear infinite',
-        shimmer: 'shimmer 1.3s linear infinite',
+        tilt: 'tilt 10s infinite linear',
+      },
+      typography: {
+        DEFAULT: {
+          // code: {
+          //   color: '#86e1fc',
+          //   '&::before': { content: `"" !important` },
+          //   '&::after': { content: `"" !important` },
+          //   fontWeight: 'normal',
+          // },
+          // '[data-rehype-pretty-code-fragment]:nth-of-type(2) pre': {
+          //   '[data-line]::before': {
+          //     content: 'counter(line)',
+          //     counterIncrement: 'line',
+          //     display: 'inline-block',
+          //     width: '1rem',
+          //     marginRight: '1rem',
+          //     textAlign: 'right',
+          //     color: colors.slate[600],
+          //   },
+          //   '[data-highlighted-line]::before': {
+          //     color: colors.slate[400],
+          //   },
+          // },
+        },
       },
     },
   },
   plugins: [require('@tailwindcss/forms')],
-} satisfies Config;
+};
