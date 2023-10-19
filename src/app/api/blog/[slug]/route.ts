e0f3c570038ee0bcd9492107c9653843581e767d
nilay nath sharan <nilay@pop-os.localdomain>
2023-10-19 09:41:16 +0530

    fix: link

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getFileBySlug } from '@/lib/mdx.server';
 
 export async function GET(req: NextRequest) {
-  const BASE_URL = `https://nilaysharan.vercel.app`;
+  const BASE_URL = `${req.nextUrl.origin}/api/blog/`;
   const url = new URL(req.url || '', BASE_URL);
 
   const slug = url.pathname.split('/').pop() || '';
@@ -13,10 +13,14 @@ export async function GET(req: NextRequest) {
   try {
     const file = await getFileBySlug('blog', slug);
 
-    if (!file) return new NextResponse(null, { status: 404 });
+    if (!file)
+      return new NextResponse(null, { status: 404, statusText: 'Not Found' });
 
     return NextResponse.json(file);
   } catch (error) {
-    return new NextResponse(null, { status: 404 });
+    return new NextResponse(null, {
+      status: 404,
+      statusText: 'Not Found Response',
+    });
   }
 }
