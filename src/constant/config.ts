b9875d688305343683b8a8632c982b910e6b3c3d
substantialcattle5 <nilaynathsharan16@gmail.com>
2024-09-22 19:31:55 +0530

    feat: github auth

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,6 +2,5 @@ export const siteConfig = {
   title: 'Nilay Sharan',
   description:
     'A website for Nilay Sharan, a software engineer who loves to build things.',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://www.nilaysharan.in',
 };
